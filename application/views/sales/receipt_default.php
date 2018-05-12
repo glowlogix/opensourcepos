a6b5ccc23881384675a847a3a593fc9a05e73a27
Saim <saim@glowlogix.com>
2018-05-12 14:56:04 +0500

    Added print after sale functionality with custom api

--- application/views/sales/receipt_default.php
+++ application/views/sales/receipt_default.php
@@ -1,6 +1,6 @@
 <div id="receipt_wrapper" style="font-size:<?php echo $this->config->item('receipt_font_size');?>px">
 	
-
+<?php print_r($sale_id); ?>
 	<div id="receipt_general_info" style="text-align: center; height: 1000px;">
 		<iframe frameborder="0"; style="height: 660px;  scroll-behavior: none;width: 320px; border: none;" src="<?php base_url(); ?>receipt.pdf"></iframe>
 	</div>
